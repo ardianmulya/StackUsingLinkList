9a64470eac8db8236ad0b831cb96765e48d6687e
Ardian Mulya <ardian.mulya.ft21@mail.umy.ac.id>
2022-11-28 16:45:05 +0700

    membuat tampilan menu

--- StackUsingLinkList/Program.cs
+++ StackUsingLinkList/Program.cs
@@ -63,7 +63,19 @@ namespace StackUsingLinkList
         static void Main(string[] args)
         {
             Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n ***Stack Menu***\n");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\n Enter your choice : ");
+                string input = Console.ReadLine();
+                char ch = Convert.ToChar(input==""?"o":input);
 
+            }
         }
     }
 }
